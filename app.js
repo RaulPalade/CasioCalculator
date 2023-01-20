47eb13b04bb95ba203f048cd853add7e58079b35
Raul Palade <raul.palade@edu.unito.it>
2023-01-20 10:26:32 +0100

    Added function to move calculator and fixed css attributes order

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
+const calculator = document.getElementById("calculator");
 const displayInput = document.querySelector(".input");
 const displayResult = document.querySelector(".result");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const dots = document.querySelectorAll(".dot");
 
+// Configuration
 let values = new Array(100).fill("");
-
 let valuesIndex = 0;
 let selectedOperator = "";
 let result = 0;
 
-// Numbers
+// Manage numbers input
 numbers.forEach((number) => {
   number.addEventListener("click", () => updateDisplay(number.textContent));
 });
 
-function updateDisplay(number) {
-  values[valuesIndex] += number;
-  addToInputDisplay();
-}
-
-// Operators
+// Manage operators
 operators.forEach((operator) => {
   operator.addEventListener("click", () => {
     let operatorType = operator.textContent;
@@ -105,6 +101,13 @@ operators.forEach((operator) => {
   });
 });
 
+// Update values array with new number and update display
+function updateDisplay(number) {
+  values[valuesIndex] += number;
+  addToInputDisplay();
+}
+
+// Update display every time a new element is added
 function addToInputDisplay() {
   let resultString = "";
   values.forEach((value) => {
@@ -126,6 +129,7 @@ function addToInputDisplay() {
   displayResult.innerHTML = resultString;
 }
 
+// Check if the last element is another operator when click on operator
 function stillOperator() {
   return (
     values[valuesIndex - 1] === "/" ||
@@ -135,16 +139,24 @@ function stillOperator() {
   );
 }
 
+// Remove last inserted number and update display
+function cancelElement() {
+  values[valuesIndex] = "";
+  addToInputDisplay();
+}
+
+// Calculate the final result of the operation
 function calculateResult() {
   let operation = "";
   values.forEach((value) => {
     operation += value;
   });
   displayInput.innerHTML = operation;
-  result = eval(operation);
+  result = eval(operation).toFixed(2);
   selectedOperator = "";
 }
 
+// Reset values of the calculator
 function resetValues() {
   values = new Array(100).fill("");
   selectedOperator = "";
@@ -152,21 +164,21 @@ function resetValues() {
   valuesIndex = 0;
 }
 
+// Reset display info
 function resetDisplay() {
   displayInput.innerHTML = "";
   displayResult.innerHTML = "";
   unfillDots();
 }
 
+// Timer for dots fill and unfill
 const timer = (ms) => new Promise((res) => setTimeout(res, ms));
-
 async function fillDots() {
   for (var i = 3; i >= 0; i--) {
     dots[i].style.backgroundColor = "lightgreen";
     await timer(200);
   }
 }
-
 async function unfillDots() {
   for (var i = 0; i < 4; i++) {
     dots[i].style.backgroundColor = "white";
@@ -174,10 +186,14 @@ async function unfillDots() {
   }
 }
 
-function cancelElement() {
-  console.log(values);
-  console.log(valuesIndex);
-  values[valuesIndex] = "";
-  console.log(values);
-  addToInputDisplay();
-}
+// Move the calculator on the page with mouse
+let width = calculator.offsetWidth;
+let height = calculator.offsetHeight;
+calculator.addEventListener("pointermove", (e) => {
+  if (e.pressure > 0) {
+    const x = e.pageX;
+    const y = e.pageY;
+    calculator.style.top = y - height / 2 + "px";
+    calculator.style.left = x - width / 2 + "px";
+  }
+});
